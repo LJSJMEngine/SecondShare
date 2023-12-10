005ed9a12b63b78ad6bceab6e38b10fad365fcc2
hyenminkim <hyenmin8922@gmail.com>
2023-12-10 23:32:29 +0900

    list 카테고리#1

--- src/main/java/com/lec/spring/service/BoardServiceImpl.java
+++ src/main/java/com/lec/spring/service/BoardServiceImpl.java
@@ -57,6 +57,12 @@ public class BoardServiceImpl implements BoardService {
 //    }
 
     @Override
+    public List<Post> searchByCategory(Long categoryId) {
+
+        return postRepository.searchByCategory(categoryId);
+    }
+
+    @Override
     public int write(Post post) {
         return 0;
     }
