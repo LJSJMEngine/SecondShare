0db3dd0a834eaa2a461e17e22b5a2a93830063bc
imsmko <kosoomin.97@gmail.com>
2023-12-13 12:12:15 +0900

    마이페이지 1차 업데이트 (프로필 보기, 프로필 수정)

--- src/main/java/com/lec/spring/service/UserServiceImpl.java
+++ src/main/java/com/lec/spring/service/UserServiceImpl.java
@@ -22,8 +22,9 @@ public class UserServiceImpl implements UserService{
 
     @Autowired
     public UserServiceImpl(SqlSession sqlSession) {
-        //userRepository = sqlSession.getMapper(UserRepository.class);
+        userRepository = sqlSession.getMapper(UserRepository.class);
         //authorityRepository = sqlSession.getMapper(AuthorityRepository.class);
+
     }
 
     @Override
@@ -58,5 +59,68 @@ public class UserServiceImpl implements UserService{
     public List<Authority> selectAuthById(Long id) {
         User user = userRepository.findById(id);
         return authorityRepository.findByUser(user);
+    }
+
+    // 마이페이지 - 프로필 보기, 프로필 수정
+    @Override
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    @Override
+    public void deleteAccount(String username) {
+        User user = userRepository.findByUsername(username);
+        if (user != null) {
+            user.setStatus(1); // 회원 탈퇴 상태로 변경
+            userRepository.deleteAccount(username); // 변경된 상태를 저장
+        }
+    }
+
+    @Override
+    public void updatePassword(String newPassword, String username) {
+        // 비밀번호 유효성 검사
+        if (!isValidPassword(newPassword)) {
+            throw new IllegalArgumentException("유효하지 않은 비밀번호 형식입니다.");
+        }
+        userRepository.updatePassword(newPassword, username);
+    }
+
+    private boolean isValidPassword(String password) {
+        // 예: 최소 8자, 영문 대소문자, 숫자, 특수문자 포함
+        String passwordRegex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$";
+        return password.matches(passwordRegex);
+    }
+
+    @Override
+    public void updatePhoneNumber(String newPhoneNumber, String username) {
+        // 핸드폰 번호 유효성 검사
+        if (!isValidPhoneNumber(newPhoneNumber)) {
+            throw new IllegalArgumentException("유효하지 않은 핸드폰 번호 형식입니다.");
+        }
+
+        // 핸드폰 번호 변경
+        userRepository.updatePhoneNumber(newPhoneNumber, username);
+    }
+
+    private boolean isValidPhoneNumber(String phoneNumber) {
+        String phoneRegex = "^\\d{3}-\\d{4}-\\d{4}$";
+        return phoneNumber.matches(phoneRegex);
+    }
+
+    @Override
+    public void updateEmailAddress(String newEmailAddress, String username) {
+        // 핸드폰 번호 유효성 검사
+        if (!isValidEmailAddress(newEmailAddress)) {
+            throw new IllegalArgumentException("유효하지 않은 이메일 형식입니다.");
+        }
+
+        // 핸드폰 번호 변경
+        userRepository.updateEmailAddress(newEmailAddress, username);
+    }
+
+    private boolean isValidEmailAddress(String emailAddress) {
+        // 이메일 정규식 (영문자와 숫자 포함)
+        String emailRegex = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z]+$";
+        return emailAddress.matches(emailRegex);
     }
 }
