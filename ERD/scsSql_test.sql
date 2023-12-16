107dd4956c3c42a595c388b473d4bcb4c7e74bba
dusdnr33 <qewrytu@naver.com>
2023-12-16 19:58:10 +0900

    ERD 수정 (채팅 로그에 보낸 유저ID 및 생성 날짜 표기)
    sender_id (User외래 키)
    createDate Datetime

--- ERD/scsSql_test.sql
+++ ERD/scsSql_test.sql
@@ -5,8 +5,8 @@ SELECT * FROM s1_comment ORDER BY id DESC;
 SELECT * FROM s1_review ORDER BY id DESC;
 SELECT * FROM s1_authority ORDER BY id DESC;
 SELECT * FROM s1_notice ORDER BY id DESC;
-
 SELECT * FROM s1_chatroom  ORDER BY room_id DESC;
+SELECT * FROM s1_chatMessage  ORDER BY room_id DESC;
 
 SELECT
 	id,
